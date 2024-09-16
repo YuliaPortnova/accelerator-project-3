f5d28862f9f4e842ff35820f3990f6a8d557cdf6
YuliaPortnova <yuyul@yandex.ru>
2024-09-16 10:42:33 +0300

    feat: завершает стилизацию шапки

--- source/js/modules/header.js
+++ source/js/modules/header.js
@@ -6,6 +6,7 @@ const list = header.querySelector('.header__menu-list');
 const accordionButtons = header.querySelectorAll('.header__list-button');
 
 const LIST_PADDING = 70;
+const SUBLIST_PADDING = 100;
 
 const onHeaderClick = (event) => {
   if(!event.target.closest('.header__menu')) {
@@ -13,19 +14,29 @@ const onHeaderClick = (event) => {
   }
 }
 
+const onButtonClick = () => {
+  list.style.maxHeight = `${list.scrollHeight + LIST_PADDING + SUBLIST_PADDING}px`;
+}
+
 const initHeader = () => {
   openButton.addEventListener('click', () => {
     const isOpen = header.classList.contains('is-open');
     if (isOpen) {
-      // list.style.maxHeight = 0;
+      list.style.maxHeight = 0;
       header.classList.remove('is-open');
       openButton.classList.remove('button--blue-background');
       header.removeEventListener('click', onHeaderClick);
+      accordionButtons.forEach((button)=> {
+        button.removeEventListener('click', onButtonClick);
+      });
     } else {
-      // list.style.maxHeight = `${list.scrollHeight + LIST_PADDING}px`;
+      list.style.maxHeight = `${list.scrollHeight + LIST_PADDING}px`;
       header.classList.add('is-open');
       openButton.classList.add('button--blue-background');
       header.addEventListener('click', onHeaderClick);
+      accordionButtons.forEach((button)=> {
+        button.addEventListener('click', onButtonClick);
+      })
     }
   });
 
