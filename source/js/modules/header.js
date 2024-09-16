73fdcf35d36a03383d60355d538d24cae7feb31c
YuliaPortnova <yuyul@yandex.ru>
2024-09-16 09:51:53 +0300

    feat: добавляет оживление и стилизацию меню WIP

--- source/js/modules/header.js
+++ source/js/modules/header.js
@@ -1,6 +1,11 @@
+import { createAccordions } from "../utils/accordions.js";
+
 const header = document.querySelector('.header');
 const openButton = header.querySelector('.header__menu-button');
 const list = header.querySelector('.header__menu-list');
+const accordionButtons = header.querySelectorAll('.header__list-button');
+
+const LIST_PADDING = 70;
 
 const onHeaderClick = (event) => {
   if(!event.target.closest('.header__menu')) {
@@ -12,17 +17,19 @@ const initHeader = () => {
   openButton.addEventListener('click', () => {
     const isOpen = header.classList.contains('is-open');
     if (isOpen) {
-      list.style.maxHeight = 0;
+      // list.style.maxHeight = 0;
       header.classList.remove('is-open');
       openButton.classList.remove('button--blue-background');
       header.removeEventListener('click', onHeaderClick);
     } else {
-      list.style.maxHeight = `${list.scrollHeight}px`;
+      // list.style.maxHeight = `${list.scrollHeight + LIST_PADDING}px`;
       header.classList.add('is-open');
       openButton.classList.add('button--blue-background');
       header.addEventListener('click', onHeaderClick);
     }
   });
+
+  createAccordions(accordionButtons);
 };
 
 export { initHeader } ;
---
+++ source/js/utils/accordions.js
@@ -0,0 +1,13 @@
+const createAccordions = (buttons) => {
+  buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      button.classList.toggle('is-active');
+
+      const clickedDescription = button.nextElementSibling;
+      const isActive = button.classList.contains('is-active');
+      clickedDescription.style.maxHeight = isActive ? `${clickedDescription.scrollHeight}px` : 0;
+    });
+  });
+}
+
+export { createAccordions };
