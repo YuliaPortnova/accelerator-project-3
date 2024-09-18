423bf2b0ef52c59f7f792fafbf1b6dc5def10a18
YuliaPortnova <yuyul@yandex.ru>
2024-09-18 11:19:44 +0300

    feat: добавляет стилизацию формы в модальном окне для мобильного

--- source/js/modules/modal.js
+++ source/js/modules/modal.js
@@ -1,19 +1,42 @@
-const closeModal = () => {
-  modal.classList.remove('.is-open');
+let onDocumentKeydown;
+
+const openModal = (modal, closeButton) => {
+  onDocumentKeydown = (event) => {
+    if (event.key.startsWith('Esc')) {
+      closeButton.click();
+    }
+  };
+
+  modal.scroll(0, 0);
+  modal.classList.add('is-open');
+  document.body.classList.add('page__modal-open');
+  document.addEventListener('keydown', onDocumentKeydown);
 }
 
-const onModalCloseButtonClick = () => {
-  closeModal();
-};
+const closeModal = (modal) => {
+  modal.classList.remove('is-open');
+  document.body.classList.remove('page__modal-open');
+  document.removeEventListener('keydown', onDocumentKeydown);
+}
 
 const createModal = (modal, openButton) => {
-  const modalCloseButton = modal.querySelector('.modal__close-button');
+  const closeButton = modal.querySelector('.modal__close-button');
 
-  openButton.addEventListener('click', () => {
-    modal.classList.add('.is-open');
-  });
+  const onCloseButtonClick = () => {
+    closeModal(modal);
+  };
+  const onOpenButtonClick = () => {
+    openModal(modal, closeButton);
+  };
+  const onModalClick = (event) => {
+    if (!event.target.closest('.modal__content')) {
+      closeModal(modal);
+    }
+  };
 
-  modalCloseButton.addEventListener('click', onModalCloseButtonClick);
+  openButton.addEventListener('click', onOpenButtonClick);
+  closeButton.addEventListener('click', onCloseButtonClick);
+  modal.addEventListener('click', onModalClick);
 };
 
 export { createModal };
