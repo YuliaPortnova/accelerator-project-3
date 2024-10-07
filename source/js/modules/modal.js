7c8afae60a90109379fae3d25e6c76373ffc21e8
YuliaPortnova <yuyul@yandex.ru>
2024-10-07 12:26:38 +0300

    feat: реализует переход фокуса при открытии/закрытии модального окна

--- source/js/modules/modal.js
+++ source/js/modules/modal.js
@@ -2,6 +2,8 @@ let onDocumentKeydown;
 let onDocumentFocus;
 
 const openModal = (modal, closeButton) => {
+  const firstTabElement = modal.querySelector('.modal__first-tab-element-js');
+
   onDocumentKeydown = (event) => {
     if (event.key.startsWith('Esc')) {
       closeButton.click();
@@ -18,27 +20,32 @@ const openModal = (modal, closeButton) => {
   document.body.classList.add('page__modal-open');
   document.addEventListener('keydown', onDocumentKeydown);
   document.addEventListener('focus', onDocumentFocus, true);
+
+  setTimeout(() => {
+    firstTabElement.focus();
+  }, '500');
 };
 
-const closeModal = (modal) => {
+const closeModal = (modal, openButton) => {
   modal.classList.remove('is-open');
   document.body.classList.remove('page__modal-open');
   document.removeEventListener('keydown', onDocumentKeydown);
   document.removeEventListener('focus', onDocumentFocus, true);
+  openButton.focus();
 };
 
 const createModal = (modal, openButton) => {
   const closeButton = modal.querySelector('.modal__close-button');
 
   const onCloseButtonClick = () => {
-    closeModal(modal);
+    closeModal(modal, openButton);
   };
   const onOpenButtonClick = () => {
     openModal(modal, closeButton);
   };
   const onModalClick = (event) => {
     if (!event.target.closest('.modal__content')) {
-      closeModal(modal);
+      closeModal(modal, openButton);
     }
   };
 
